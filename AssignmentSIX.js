f7d56e2211992c61927448a8cf1fd781fec574e4
ibtidaifreet <ibtida.ifreet@nyu.edu>
2021-04-10 00:10:22 -0400

    assignment six css and js and index  update

---
+++ AssignmentSIX.js
@@ -0,0 +1,11 @@
+function buttonInstagram() {
+    document.getElementById('text').innerText = "Because of the fact that Instagram is primarily a visual medium, that is the baseline that everything is speculated up on. People are judged through their photos. Their sense of self worth tied to likes and comments. But does having the most perfect pictures mean that you have the most perfect life?"
+}
+
+function buttonTwitter() {
+    document.getElementById('text').innerText = "Twitter has changed the course of history. Now all of a sudden, rallies have built up with the power of hashtags. People have united and the word is spread. But how many people does the message truly resonate with? How many are there to just jump on the woke train?"
+}
+
+function buttonFacebook() {
+    document.getElementById('text').innerText = "Somehow at in the middle of the late 2000's your whole life is up for show-- your friends, your family. Photos taken in private all uploaded, and your name. But if I were friends with you on Facebook, does that mean that I am friends with you in real life too?"
+}
